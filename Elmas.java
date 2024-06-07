55449738e671354382fd6a77daf3b4db00189d81
İrem Sıla Boyraz <147523018+iremsilaboyraz@users.noreply.github.com>
2024-06-07 17:01:43 +0300

    Add files via upload

---
+++ Elmas.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Elmas {
+    public static void main (String[] args){
+    Scanner input=new Scanner(System.in);
+    System.out.print("Basamak sayısı giriniz:");
+    int n= input.nextInt();
+
+    for (int i=1;i<=n;i++){
+        for (int k = 1; k<= (n-i) ; k++){
+            System.out.printf(" ");
+        }
+        for (int j =1 ; j <= (2*i) - 1 ;j++){
+            System.out.print("*");
+        }
+        System.out.println(" ");
+
+    }
+        for (int i=n-1;i>=1;i--){
+            for (int k = 1; k<= (n-i) ; k++){
+                System.out.printf(" ");
+            }
+            for (int j =1 ; j <= (2*i) -1;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+
+        }
+}
+}
+
+
